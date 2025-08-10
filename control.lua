52f815c0fb3e77674765283e3dbb27ce6e0b125e
Braxbro <63511112+Braxbro@users.noreply.github.com>
2025-08-09 23:52:45 -0400

    fix logging and alerts

--- control.lua
+++ control.lua
@@ -1,4 +1,4 @@
-if not (settings.startup["pfu-debug-mode"] or script.active_mods["debugadapter"]) then
+if not (settings.startup["pfu-debug-mode"].value or script.active_mods["debugadapter"]) then
     script.on_nth_tick(60 * 60 * 10, function()
         game.print("Prototype Format Updater is active in patching mode.")
         game.print("Please give back to the modding community by reporting any errors this mod patches.")
--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -3,7 +3,7 @@ local isDebug = settings.startup["rfu-debug-mode"] or mods["debugadapter"]
 
 for name, prototype in pairs(data.raw["recipe"]) do
     if prototype.result then
-        print(prefix .. "Warning: Nonmigrated recipe " .. name .. " found.")
+        log(prefix .. "Warning: Nonmigrated recipe " .. name .. " found.")
         local old = table.deepcopy(prototype)
         prototype.results = {
             {
@@ -14,14 +14,14 @@ for name, prototype in pairs(data.raw["recipe"]) do
         }
         prototype.result = nil
         prototype.result_count = nil
-        print(
+        log(
             prefix .. "Successfully migrated recipe " .. name .. " from:\n" ..
             serpent.block(old) ..
             "\nto:\n" ..
             serpent.block(prototype)
         )
         if isDebug then
-            print(prefix .. "DEBUG: Reverting migration.")
+            log(prefix .. "DEBUG: Reverting migration.")
             data.raw["recipe"][name] = old
         end
     end
